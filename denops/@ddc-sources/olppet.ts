5ff4f5e72e11d5cbf9746ed4a83a51d5a1c6cfc0
TamDik <dik.tkmr.1102@gmail.com>
2022-01-23 21:04:36 +0900

    Add getCompletePosition

--- denops/@ddc-sources/olppet.ts
+++ denops/@ddc-sources/olppet.ts
@@ -1,4 +1,5 @@
-import { Denops, BaseSource, Candidate } from '../olppet/deps.ts'
+import { Denops, BaseSource, Candidate, Context } from '../olppet/deps.ts'
+
 
 type Params = Record<string, never>;
 type UserData = {word: string};
@@ -7,6 +8,15 @@ export class Source extends BaseSource<Params, UserData> {
     async gatherCandidates(args: {denops: Denops}): Promise<Candidate<UserData>[]> {
         const candidates = await args.denops.dispatch('olppet', 'getCandidates') as {word: string, menu?: string}[];
         return candidates;
+    }
+
+    getCompletePosition(args: {context: Context}): Promise<number> {
+        const triggerMatch = args.context.input.match(/(?<!\w)\S*$/);
+        if (!triggerMatch) {
+            return super.getCompletePosition(args);
+        }
+        const triggerLength = triggerMatch[0].length;
+        return Promise.resolve(args.context.input.length - triggerLength);
     }
 
     params(): Params {
